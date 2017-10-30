5f025941531705b68435a1afb152fdb011bd27a9
rasika <rasikapohankar@gmail.com>
2017-10-30 16:30:09 -0400

    Create dataframe of TPM against label

--- test1.py
+++ test1.py
@@ -9,36 +9,39 @@ import os
 #import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import ward, dendrogram,linkage,fcluster,cophenet,distance
 import scipy.cluster.hierarchy as hier
+
 if __name__=='__main__':
+    labels = {}
+
     raw_path=['/home/rasika/Documents/Computational Biology/Project/']
     output_path='/home/rasika/Documents/Computational Biology/Project/Results'
-    colnames = ['Name', 'Length', 'EffectiveLength', 'TPM', 'NumReads']
-    colnames1 = ['Name', 'TPM']
-    colnames0 = ['Name', 'TPM']
+    colnames1 = ['TPM']
     result =[]
-    f1=pd.read_csv('/home/rasika/Documents/Computational Biology/Project/Data/Train/ERR188021/bias/quant.sf', sep='\t',header = 0,dtype='unicode')
-    f1.to_csv('/home/rasika/Documents/Computational Biology/Project/Results/Resource.csv', index=False)
-    metadata= pd.read_csv('/home/rasika/Documents/Computational Biology/Project/Results/Resource.csv', usecols=colnames0,converters={'Name':str, 'TPM':float})
-    #metadata = metadata.rename(columns={'TPM': 'TPM_1'})
-    print metadata.head()
+
+    merged_df = pd.DataFrame()
+
+    labels_data = pd.read_csv(raw_path[0] +'/' + 'p1_train.csv', sep='\t',header = 0,dtype='unicode')
+
+    for row in labels_data.itertuples():
+        key = row[1].split(',')[0]
+        value = row[1].split(',')[1]
+        labels[key] = value
+    
 
     for path in raw_path:
         for roots,dir,files in os.walk(path):
-            #print ("Files in: " + roots[:])
             directories = roots.split("/")
             directory_name = directories[-1]
             output_filename = directories[-2]
-            #print directory_name
             if directory_name == "bias":
                 for filename in files:
-                    #print filename
 
                     if filename == 'quant.sf':
                         data = pd.read_csv(roots+'/'+filename, sep='\t',header = 0,dtype='unicode')
                         data.to_csv(output_path+'/'+output_filename+'.csv', index=False)
+                        data = pd.read_csv(output_path+'/'+output_filename+'.csv', usecols=colnames1,converters={'TPM':float})
+                        data['label'] = labels[output_filename]
 
-                        data = pd.read_csv(output_path+'/'+output_filename+'.csv', usecols=colnames1,converters={'Name':str,'TPM':float})
+                        merged_df = merged_df.append(data)
 
-    data = data.merge(metadata, on='Name', how='inner')
-    print data.head()
-    print metadata.head()
+    print merged_df.head()
