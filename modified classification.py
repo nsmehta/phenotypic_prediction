30bd7c9ca90204b30baea4df7c4fee48469c3c25
rasika <rasikapohankar@gmail.com>
2017-11-03 18:41:31 -0400

    Include code for taking paths from command line

--- modified classification.py
+++ modified classification.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 import sys
 from sklearn.feature_extraction import DictVectorizer
 import os
-import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import ward, dendrogram,linkage,fcluster,cophenet,distance
 import scipy.cluster.hierarchy as hier
 from sklearn.ensemble import RandomForestClassifier
@@ -17,35 +16,45 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
 from sklearn.decomposition import PCA
+
 if __name__=='__main__':
     colnames1 = ['TPM']
 
     classifier_input = list()
-    raw_path = ['C:\\Users\\aditi\\Desktop\\Comp Bio\\train']
-    output_path = 'C:\\Users\\aditi\\Desktop\\Comp Bio\\Results'
-    train_path = 'C:\\Users\\aditi\\Desktop\\Comp Bio\\p1_train.csv'
+
+    raw_path_string = raw_input("Enter Raw Path:")
+    output_path = raw_input("Enter Output Path:")
+    train_path = raw_input("Enter path of training data:")
+    print train_path
+    split_on_string = "/"
+    raw_path = [raw_path_string]
+
+    # raw_path = ['C:\\Users\\aditi\\Desktop\\Comp Bio\\train']
+    # output_path = 'C:\\Users\\aditi\\Desktop\\Comp Bio\\Results'
+    # train_path = 'C:\\Users\\aditi\\Desktop\\Comp Bio\\p1_train.csv'
     train_data = pd.read_csv(train_path, sep=',', header=0, dtype='unicode')
 
     for path in raw_path:
         for roots,dir,files in os.walk(path):
-            directories = roots.split("\\")
+            directories = roots.split(split_on_string)
             directory_name = directories[-1]
             output_filename = directories[-2]
             if directory_name == "bias":
                 for filename in files:
                     if filename == 'quant.sf':
-                        data = pd.read_csv(roots + '\\' + filename, sep='\t', header=0, dtype='unicode')
-                        data.to_csv(output_path + '\\' + output_filename + '.csv', index=False)
-                        data = pd.read_csv(output_path + '\\' + output_filename + '.csv', usecols=colnames1,converters={'TPM': float})
+                        data = pd.read_csv(roots + split_on_string + filename, sep='\t', header=0, dtype='unicode')
+                        data.to_csv(output_path + split_on_string + output_filename + '.csv', index=False)
+                        data = pd.read_csv(output_path + split_on_string + output_filename + '.csv', usecols=colnames1,converters={'TPM': float})
                         data_list = [output_filename] + data.TPM.tolist()
                         classifier_label =train_data[train_data['accession'].isin([output_filename])]["label"].tolist()[0]
                         data_list = data_list + [classifier_label]
                         classifier_input.append(data_list[:])
+
+
 df = pd.DataFrame(classifier_input)
-features = df.columns[1:199325]
-y, label = pd.factorize(df[199325])
+
 X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, 1:-1], df.iloc[:, -1], test_size = 0.30, random_state = 0)
-clf = RandomForestClassifier(n_jobs=-1, random_state=0, n_estimators= 10, max_features=None, criterion="gini")
+clf = RandomForestClassifier(n_jobs=-1, random_state=0, n_estimators= 10, max_features='auto', criterion="gini")
 clf.fit(X_train, y_train)
 preds = clf.predict(X_test)
 print "F1 score",f1_score(y_test,preds ,average='macro')
