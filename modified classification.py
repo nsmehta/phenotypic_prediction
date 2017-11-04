ccce3355ad91c0d08eec98ccd6fbd5838f9aa4f0
rasika <rasikapohankar@gmail.com>
2017-11-04 00:05:15 -0400

    Separate classifiers into methods

--- modified classification.py
+++ modified classification.py
@@ -16,6 +16,38 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
 from sklearn.decomposition import PCA
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+
+
+def prediction_with_random_forest(df):
+    X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, 1:-1], df.iloc[:, -1], test_size = 0.30, random_state = 0)
+    random_forest_classifier(X_train, y_train, X_test, y_test)
+
+
+def prediction_with_tree_classifier(df):
+    clf = ExtraTreesClassifier()
+    clf = clf.fit(df.iloc[:, 1:-1],df.iloc[:, -1] )
+    clf.feature_importances_
+    model = SelectFromModel(clf, threshold="median", prefit=True)
+    X_new = model.transform(df.iloc[:, 1:-1])
+    print X_new.shape
+
+    X_train, X_test, y_train, y_test = train_test_split(X_new, df.iloc[:, -1], test_size = 0.30, random_state = 0)
+    random_forest_classifier(X_train, y_train, X_test, y_test)
+
+
+def random_forest_classifier(X_train, y_train, X_test, y_test):
+    clf = RandomForestClassifier(n_jobs=-1, random_state=0, n_estimators= 10, max_features='auto', criterion="gini")
+    clf.fit(X_train, y_train)
+    preds = clf.predict(X_test)
+    print "F1 score",f1_score(y_test,preds ,average='macro')
+    scores=cross_val_score(clf,df.iloc[:, 1:-1], df.iloc[:, -1],cv=5,scoring='f1_macro')
+    print "F1scores with 5 fold cross validation", scores
+    print " mean of score i.e accuracy", scores.mean()
+    print "accuracy score without k-fold", accuracy_score(preds,y_test)
+
+
 
 if __name__=='__main__':
     colnames1 = ['TPM']
@@ -25,13 +57,10 @@ if __name__=='__main__':
     raw_path_string = raw_input("Enter Raw Path:")
     output_path = raw_input("Enter Output Path:")
     train_path = raw_input("Enter path of training data:")
-    print train_path
+
     split_on_string = "/"
     raw_path = [raw_path_string]
 
-    # raw_path = ['C:\\Users\\aditi\\Desktop\\Comp Bio\\train']
-    # output_path = 'C:\\Users\\aditi\\Desktop\\Comp Bio\\Results'
-    # train_path = 'C:\\Users\\aditi\\Desktop\\Comp Bio\\p1_train.csv'
     train_data = pd.read_csv(train_path, sep=',', header=0, dtype='unicode')
 
     for path in raw_path:
@@ -44,7 +73,8 @@ if __name__=='__main__':
                     if filename == 'quant.sf':
                         data = pd.read_csv(roots + split_on_string + filename, sep='\t', header=0, dtype='unicode')
                         data.to_csv(output_path + split_on_string + output_filename + '.csv', index=False)
-                        data = pd.read_csv(output_path + split_on_string + output_filename + '.csv', usecols=colnames1,converters={'TPM': float})
+                        data = pd.read_csv(output_path + split_on_string + output_filename + '.csv', usecols=colnames1,converters={'TPM': float})                        
+                        # data = data[:, 3]
                         data_list = [output_filename] + data.TPM.tolist()
                         classifier_label =train_data[train_data['accession'].isin([output_filename])]["label"].tolist()[0]
                         data_list = data_list + [classifier_label]
@@ -52,13 +82,5 @@ if __name__=='__main__':
 
 
 df = pd.DataFrame(classifier_input)
-
-X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, 1:-1], df.iloc[:, -1], test_size = 0.30, random_state = 0)
-clf = RandomForestClassifier(n_jobs=-1, random_state=0, n_estimators= 10, max_features='auto', criterion="gini")
-clf.fit(X_train, y_train)
-preds = clf.predict(X_test)
-print "F1 score",f1_score(y_test,preds ,average='macro')
-scores=cross_val_score(clf,df.iloc[:, 1:-1], df.iloc[:, -1],cv=5,scoring='f1_macro')
-print "score with cross validation", scores
-print " mean score", scores.mean()
-print "accuracy score without k-fold", accuracy_score(preds,y_test)
+#prediction_with_random_forest(df)
+prediction_with_tree_classifier(df)
